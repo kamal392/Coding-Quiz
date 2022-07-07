688337550d7f85766450eece701d9ae3c7a77096
kamal kishore <kamal.kishore981@gmail.com>
2022-07-07 03:41:39 -0400

    css

---
+++ assets/script.js
@@ -0,0 +1,45 @@
+let timeEl = document.querySelector("p.time");
+
+let secondsLeft = 75;
+
+let scoreEl = document.querySelector("#score");
+
+const introEl = document.querySelector("#intro");
+
+const questionsEl = document.querySelector("#questions");
+
+let questionEl = document.querySelector("#question");
+
+let questionCount = 0;
+
+const yaynayEl = document.querySelector("#yaynay");
+
+const finalEl = document.querySelector("final");
+
+let initialsInput = document.querySelector("#initials");
+
+const highscoresEl = document.querySelector("#highscore");
+
+let scoreListEl = document.querySelector("#score-list");
+
+let scoreList = [];
+
+const startBtn = document.querySelector("#start");
+
+const ansBtn = document.querySelectorAll("buttonansBtn");
+
+const ans1Btn = document.querySelector("#answer1");
+
+const ans2Btn = document.querySelector("#answer1");
+
+const ans3Btn = document.querySelector("#answer1");
+
+const ans4Btn = document.querySelector("#answer1");
+
+const submitScrBtn = document.querySelector("#submit-score");
+
+const goBackBtn = document.querySelector("#goback");
+
+const clearScrBtn = document.querySelector("#clearscores");
+
+const viewScrBtn = document.querySelector("#view-scores");
